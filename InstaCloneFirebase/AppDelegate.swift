ba302b876cba466c59003a35e376417a36cefc8b
zalio <orulluoglu.zeynel@gmail.com>
2020-02-03 00:37:55 +0300

    gul ki sevgilim...

--- InstaCloneFirebase/AppDelegate.swift
+++ InstaCloneFirebase/AppDelegate.swift
@@ -34,6 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    //ben bi tek seni eski gunleri isterim canım anlasana
+    //bu sabah yatagın bos kısmını resimlerinle süsledim.........
 
 }
 
